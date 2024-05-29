670a954795ec94c8b30ee7cb7003db087edfb5d5
AlejandraGaspar <168685269+AlejandraGaspar@users.noreply.github.com>
2024-05-29 12:17:10 -0600

    Creación de Seccion.cs

---
+++ Seccion.cs
@@ -0,0 +1,7 @@
+public class nuevaSeccion
+{
+  public int IdSeccion {get; set;}
+  public string NombreSeccion {get; set;}
+  public string Editorial {get; set;}
+  public float Precio {get; set;}
+}
